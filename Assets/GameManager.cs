d1e392de9a8e76fb868d562c7a4a01da213ba114
Gordon Alexander MacPherson <gordon.a.macpherson@gmail.com>
2018-02-25 14:34:27 +0000

    Fixed copyright

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,7 +1,7 @@
 ﻿// LICENSE
 // GNetworking, SimpleUnityClient and GameServer are property of Gordon Alexander MacPherson
-// No warantee is provided with this code, and no liability shall be granted under any circumstances.
-// All rights reserved GORDONITE LTD 2018 � Gordon Alexander MacPherson.
+// No warranty is provided with this code, and no liability shall be granted under any circumstances.
+// All rights reserved GORDONITE LTD 2018 (c) Gordon Alexander MacPherson.
 
 using System.Collections;
 using System.Collections.Generic;
